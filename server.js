b22aa46d3243a78412b9d06b0c2cfe16419f1bc0
Himanshu Saxena <contact@hellohimanshu.com>
2020-10-31 11:19:42 +1030

    updated routing fro express

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const path = require("path");
+
 
 const app = express();
 
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// roputes for html pages
 app.get("/", (req,res)=> {
-    res.send("Server Ready");
+    res.sendFile(path.join(__dirname, "index.html"));
 })
+
+app.get("/tables", (req,res)=> {
+    res.sendFile(path.join(__dirname, "tables.html"));
+})
+
+app.get("/reservation", (req,res)=> {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+})
+
 
 app.listen(PORT, ()=> {
     console.log(`Server listening on: http://localhost: ${PORT}`);
@@ -25,3 +41,6 @@ server.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT);
 })
 */
+//tables.html
+//index.html
+//reservation.html
