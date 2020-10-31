9a4dc182e490c8873c48e63029b637bca12a2cb5
Callum White <callumwhite210@gmail.com>
2020-10-31 10:54:49 +1030

    routing

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 let http = require("http");
+let express = require("express");
 
+let app = express();
 let PORT = 3000;
 
-function handleRequest(request, response){
-    response.end("working: path hit"+ request.url);
-}
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-let server = http.createServer(handleRequest);
+let tables = [];
 
-server.listen(PORT, function(){
-    console.log("Server listening on: http://localhost:" + PORT);
-})
+app.get("/", function(require, response){
+    response.sendFile(path.join(__dirname,""));
+});
+
+app.listen(PORT,function(){
+    console.log("App listening on PORT " + PORT);
+});
