00e9324bbe97fb2b19cadc14ad2c390bd6cbdd79
Callum White <callumwhite210@gmail.com>
2020-10-31 11:33:50 +1030

    fix

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require("express");
 const app = express();
 
 const PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.get("/", (req,res)=> {
-    res.send("Server Ready");
-})
+    res.json(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, ()=> {
     console.log(`Server listening on: http://localhost: ${PORT}`);
